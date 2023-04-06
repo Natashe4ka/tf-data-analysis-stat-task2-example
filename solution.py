78754f619a3d8fe7fc86aada8ba47a24c58f7659
Natashe4ka <90340759+Natashe4ka@users.noreply.github.com>
2023-04-07 00:23:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,18 +3,19 @@ import numpy as np
 
 from scipy.stats import norm
 
+from scipy.stats import erlang
 
 chat_id = 965404933 # Ваш chat ID, не меняйте название переменной
 
+
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     t = 41
     alpha = 1 - p
     c = t ** 2 / 2
     tau = norm.ppf(1 - alpha / 2)
     tau_2 = norm.ppf(alpha / 2)
-    left = 2*((tau_2+0.5) / np.sqrt(x.shape[0]) + x.mean()/2) / c
-    right = 2*((tau+0.5) / np.sqrt(x.shape[0]) + x.mean()/2) / c
+    t_0 = erlang.ppf(1 - alpha / 2, x.shape[0],loc=-0.5, scale=1 / x.shape[0])
+    t_1 = erlang.ppf(alpha / 2, x.shape[0],loc=-0.5, scale=1 / x.shape[0])
+    left = (t_1 + x.mean()) / c
+    right = (t_0 + x.mean()) / c
     return left, right
